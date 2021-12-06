67751bf6d5333a1c00c000d2c6024f134b558c6b
Amy Simpson <aesimpson16@gmail.com>
2021-12-06 13:42:49 -0800

    adding press page

---
+++ web/.next/server/chunks/4645.js
@@ -0,0 +1,196 @@
+exports.id = 4645;
+exports.ids = [4645];
+exports.modules = {
+
+/***/ 267:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+const sanityClient = __webpack_require__(8809);
+
+const client = sanityClient({
+  projectId: '76e3r62u',
+  // you can find this in sanity.json
+  dataset: 'production',
+  apiVersion: '2021-10-06',
+  useCdn: true // `false` if you want to ensure fresh data
+
+});
+module.exports = client;
+
+/***/ }),
+
+/***/ 4645:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "zl": () => (/* binding */ indexQuery),
+/* harmony export */   "JR": () => (/* binding */ pageQuery),
+/* harmony export */   "A3": () => (/* binding */ pageSlugsQuery),
+/* harmony export */   "oM": () => (/* binding */ eventsPastQuery),
+/* harmony export */   "_D": () => (/* binding */ eventsUpcomingQuery),
+/* harmony export */   "Dp": () => (/* binding */ eventsFeaturedQuery),
+/* harmony export */   "X6": () => (/* binding */ pressNewsQuery),
+/* harmony export */   "d9": () => (/* binding */ pressQuery),
+/* harmony export */   "ad": () => (/* binding */ pressFeaturedQuery)
+/* harmony export */ });
+/* unused harmony exports postsQuery, postQuery, postSlugsQuery, authorQuery, authorPostsQuery, postsByTagQuery, getAllPostSlugs, getPostsByTagSlug */
+/* harmony import */ var _client__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(267);
+/* harmony import */ var _client__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_client__WEBPACK_IMPORTED_MODULE_0__);
+
+
+const homeID = `*[_id == "global-config"][0].frontpage->_id`;
+const eventFields = `
+    featured,
+    datetime,
+    time_zone,
+    name,
+    type,
+    url,
+    location
+`;
+const pressFields = `
+    article_url,
+    background,
+    date,
+    featured,
+    featured_order,
+    headline,
+    publication_icon,
+    publication_source,
+    type
+`;
+const pageFields = `
+title,
+"slug": slug.current,
+"sections": sections[]{
+    _key,
+    background_color,
+    "modules": modules.modules[]{
+        _type == "reusable_modules_module" => module_ref->.module{
+            "ref_modules": modules[]{
+                ...
+            }
+        },
+        _type != "reusable_modules_module" => {
+            ...
+        }
+    }
+}
+`;
+const authorFields = `
+_id,
+avatar,
+bio,
+name,
+slug
+`;
+const postsFields = `
+_createdAt, 
+slug,
+author->{
+${authorFields}
+},
+"plaintextBody": pt::text(body),
+"estimatedReadingTime": round(length(pt::text(body)) / 5 / 180 ),
+featured_image, 
+tags, 
+title
+`;
+const indexQuery = `
+*[_type == "page" && _id == ${homeID}][0] {
+  ${pageFields}
+}`;
+const pageQuery = `{
+    "page": *[_type == "page" && slug.current == $slug && _id != ${homeID}][0] {
+        ${pageFields}
+    }
+}`;
+const pageSlugsQuery = `
+*[_type == "page" && defined(slug.current)][].slug.current
+`;
+const eventsPastQuery = `
+*[_type == "event" && datetime < now()]| order(datetime desc){
+    ${eventFields}
+}
+`;
+const eventsUpcomingQuery = `
+*[_type == "event" && datetime > now()]| order(datetime asc){
+    ${eventFields}
+}
+`;
+const eventsFeaturedQuery = `
+*[_type == "event" && datetime > now() && featured == true][0..2]| order(datetime asc){
+    ${eventFields}
+}
+`;
+const pressNewsQuery = `
+*[_type == "press" && type == 'News']| order(date desc){
+    ${pressFields}
+}
+`;
+const pressQuery = `
+*[_type == "press" && type != 'News']| order(date desc){
+    ${pressFields}
+}
+`;
+const pressFeaturedQuery = `
+*[_type == "press" && featured == true][0..2]| order(featured_order asc){
+    ${pressFields}
+}
+`;
+const postsQuery = `
+*[_type == "post"]| order(_createdAt desc){
+    ${postsFields}
+}
+`;
+const postQuery = `{
+    "post": *[_type == "post" && slug.current == $slug][0] {
+        ${postsFields}, body
+    }
+}`;
+const postSlugsQuery = (/* unused pure expression or super */ null && (`
+*[_type == "post" && defined(slug.current)][].slug.current
+`));
+const authorQuery = `{
+    "author": *[_type == "author" && slug.current == $slug][0]{
+        ${authorFields},
+        "posts": *[_type=='post' && references(^._id)]{
+            ${postsFields}
+        }
+    }
+}`;
+const authorPostsQuery = (/* unused pure expression or super */ null && (`
+    *[_type == "author" && defined(slug.current)][].slug.current
+`));
+const postsByTagQuery = `{
+    "posts": *[_type == "post" && $tag in tags[].value]{
+        ${postsFields}
+    }
+}`;
+async function getAllPostSlugs() {
+  let data = await client.fetch(`*[_type == "post" && defined(tags)][].tags[].value`);
+  data = [...new Set(data)];
+  const paths = data.map(tag => {
+    return slugify(tag);
+  });
+  return paths;
+}
+async function getPostsByTagSlug(slug) {
+  let data = await client.fetch(`*[_type == "post" && defined(tags)][].tags[].value`);
+  data = [...new Set(data)];
+  const tags = data.map(tag => {
+    let tagObj = {
+      slug: slugify(tag),
+      tag: tag
+    };
+    return tagObj;
+  });
+  const tag = tags.find(tag => tag.slug === slug);
+  return tag;
+}
+
+/***/ })
+
+};
+;
--- web/libs/queries.js
+++ web/libs/queries.js
@@ -1,3 +1,6 @@
+import slugify from '../libs/slugify';
+import client from '../client';
+
 const homeID = `*[_id == "global-config"][0].frontpage->_id`;
 const eventFields = `
     featured,
@@ -7,6 +10,18 @@ const eventFields = `
     type,
     url,
     location
+`
+
+const pressFields = `
+    article_url,
+    background,
+    date,
+    featured,
+    featured_order,
+    headline,
+    publication_icon,
+    publication_source,
+    type
 `
 
 const pageFields = `
@@ -26,6 +41,27 @@ title,
         }
     }
 }
+`
+
+const authorFields = `
+_id,
+avatar,
+bio,
+name,
+slug
+`
+
+const postsFields = `
+_createdAt, 
+slug,
+author->{
+${authorFields}
+},
+"plaintextBody": pt::text(body),
+"estimatedReadingTime": round(length(pt::text(body)) / 5 / 180 ),
+featured_image, 
+tags, 
+title
 `
 
 export const indexQuery = `
@@ -58,3 +94,78 @@ export const eventsFeaturedQuery = `
     ${eventFields}
 }
 `
+export const pressNewsQuery = `
+*[_type == "press" && type == 'News']| order(date desc){
+    ${pressFields}
+}
+`
+
+export const pressQuery = `
+*[_type == "press" && type != 'News']| order(date desc){
+    ${pressFields}
+}
+`
+
+export const pressFeaturedQuery = `
+*[_type == "press" && featured == true][0..2]| order(featured_order asc){
+    ${pressFields}
+}
+`
+
+export const postsQuery = `
+*[_type == "post"]| order(_createdAt desc){
+    ${postsFields}
+}
+`
+export const postQuery = `{
+    "post": *[_type == "post" && slug.current == $slug][0] {
+        ${postsFields}, body
+    }
+}`
+
+export const postSlugsQuery = `
+*[_type == "post" && defined(slug.current)][].slug.current
+`
+
+export const authorQuery = `{
+    "author": *[_type == "author" && slug.current == $slug][0]{
+        ${authorFields},
+        "posts": *[_type=='post' && references(^._id)]{
+            ${postsFields}
+        }
+    }
+}`
+
+export const authorPostsQuery = `
+    *[_type == "author" && defined(slug.current)][].slug.current
+`
+
+export const postsByTagQuery = `{
+    "posts": *[_type == "post" && $tag in tags[].value]{
+        ${postsFields}
+    }
+}`
+
+export async function getAllPostSlugs() {
+    let data = await client.fetch(`*[_type == "post" && defined(tags)][].tags[].value`);
+    data = [...new Set(data)];
+    const paths = data.map((tag) => {
+        return slugify(tag)
+    })
+    return paths
+}
+
+export async function getPostsByTagSlug(slug){
+    let data = await client.fetch(`*[_type == "post" && defined(tags)][].tags[].value`);
+    data = [...new Set(data)];
+    const tags = data.map((tag) => {
+        let tagObj ={
+            slug: slugify(tag),
+            tag: tag
+        }
+        return tagObj
+    })
+    const tag = tags.find(tag => tag.slug === slug);
+
+    return tag
+}
